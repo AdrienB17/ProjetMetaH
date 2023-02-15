2ea59cb4c3a9ff55cfce4f754e80e6f02cfe57aa
ableau <adrienbleau08@gmail.com>
2023-02-15 17:54:40 +0100

    Enumération v2 qui a l'air de fonctionner

--- Code/Enumeration.py
+++ Code/Enumeration.py
@@ -1,5 +1,6 @@
 import classGraph
-
+from itertools import combinations
+from math import ceil, floor
 def partition_graph(graph):
     def check_valid_partition(partition, graph):
         # Calculate the sum of weights between vertices in different classes
@@ -37,3 +38,60 @@ def partition_graph(graph):
     partition = [-1] * graph.nb_nodes
     # Call the enumeration function
     return enumerate_partitions(partition, 0, graph)
+
+
+
+def partition_graphv2(graph):
+    nb_nodes = graph.nb_nodes
+    nb_edges = graph.nb_edges
+    max_nodes_per_partition = ceil(nb_nodes/2) + floor(0.01 * nb_nodes)
+    matrice_ponderation = calculate_weight_matrix(graph)
+    min_node = min(graph.graphRep.keys())
+
+    if min_node == 0:
+        partition_1 = set(range(0, max_nodes_per_partition))
+        partition_2 = set(range(max_nodes_per_partition, nb_nodes))
+        sum_edge_weights = sum([matrice_ponderation[i][j] for i in partition_1 for j in partition_2])
+    else:
+        partition_1 = set(range(1, max_nodes_per_partition+1))
+        partition_2 = set(range(max_nodes_per_partition+1, nb_nodes+1))
+        sum_edge_weights = sum([matrice_ponderation[i-1][j-1] for i in partition_1 for j in partition_2])
+
+
+    for _ in range(10):
+        moved = True
+        while moved:
+            moved = False
+            max_gain = 0
+            best_i, best_j = None, None
+            for i in partition_1:
+                for j in partition_2:
+                    if min_node == 0:
+                        gain = matrice_ponderation[i][j] - matrice_ponderation[j][i]
+                        if gain > max_gain:
+                            max_gain = gain
+                            best_i = i
+                            best_j = j
+                    else:
+                        gain = matrice_ponderation[i-1][j-1] - matrice_ponderation[j-1][i-1]
+                        if gain > max_gain:
+                            max_gain = gain
+                            best_i = i
+                            best_j = j
+            if max_gain > 0:
+                partition_1.remove(best_i)
+                partition_1.add(best_j)
+                partition_2.remove(best_j)
+                partition_2.add(best_i)
+                sum_edge_weights += max_gain
+                moved = True
+    return partition_1, partition_2, sum_edge_weights
+
+def calculate_weight_matrix(graph):
+    nb_nodes = graph.nb_nodes
+    weight_matrix = [[0 for _ in range(nb_nodes)] for _ in range(nb_nodes)]
+    min_node = min(graph.graphRep.keys())
+    for i, edges in graph.graphRep.items():
+        for j, weight in edges.items():
+            weight_matrix[i - min_node][j - min_node] = weight
+    return weight_matrix
